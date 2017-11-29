9fa47814dbea431dd742e4943a18372cb97aacfb
4ohc <4ohc@wetigers.org>
2017-11-29 10:48:01 -0800

    added will to live
    end me please

--- Notes.py
+++ Notes.py
@@ -1,23 +1,23 @@
-#this is working
-print ("hello world")
-print ("Welcome to class")
-# This is a comment.
-# I can use this to type in text
-# but it will never run
-"""
-i wanted to type out a whole bunch
-but this is all
-text
-"""
-# variables
-a = 5
-b = 4
-c = "the quick brown fox jumps over the lazy dog"
-print (a + b)
-print (a - 5 * b)
-print (a)
-print (c)
-print (b)
-print (a * b + a - a)
-print (a / b)
-print ( a ** b) #a to the power of b
+# #this is working
+# print ("hello world")
+# print ("Welcome to class")
+# # This is a comment.
+# # I can use this to type in text
+# # but it will never run
+# """
+# i wanted to type out a whole bunch
+# but this is all
+# text
+# """
+# # variables
+# a = 5
+# b = 4
+# c = "the quick brown fox jumps over the lazy dog"
+# print (a + b)
+# print (a - 5 * b)
+# print (a)
+# print (c)
+# print (b)
+# print (a * b + a - a)
+# print (a / b)
+# print ( a ** b) #a to the power of b
